0074d1bfbf728f2b2faf3030cc82f1262b524b41
Mingfun Wong <mingfun.wong.chn@gmail.com>
2021-09-12 08:10:56 +0000

    update

--- start.sh
+++ start.sh
@@ -1,4 +1,10 @@
 GITHUB=git@github.com:andy-backup/fibos.git
+
+mkdir /root/.ssh/
+cp /data/.ssh/id_rsa /root/.ssh
+cp /data/.ssh/id_rsa.pub /root/.ssh
+chmod 400 /root/.ssh/id_rsa /root/.ssh/id_rsa.pub
+
 echo -e "Host github.com\n\tStrictHostKeyChecking no\n" > ~/.ssh/config
 git clone --depth=1 $GITHUB ./github
 git config --global user.email "mingfun.wong.chn@gmail.com"
