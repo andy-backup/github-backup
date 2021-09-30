c69f5d0c8c72457e01ba822d1675e2cd8656ecf5
Mingfun Wong <mingfun.wong.chn@gmail.com>
2021-09-30 06:58:26 +0000

    update

--- cron.sh
+++ cron.sh
@@ -1,13 +1,18 @@
 #!/bin/sh
 
 date
-wget $SNAPSHOT_URL
+wget $SNAPSHOT_URL -O /data/snapshot.log
 SNAPSHOT_FOLDER=/data/local
 SNAPSHOT_FILE=$(ls $SNAPSHOT_FOLDER/*.bin -t | head -1)
 cp $SNAPSHOT_FILE /data/github/snapshot.bin
 cd /data/github
-git add .
-git commit -m "update"
-git push
+
+git checkout --orphan latest_branch
+git add -A
+git commit -am "update"
+git branch -D master
+git branch -m master
+git push -f origin master
+
 rm -f $SNAPSHOT_FOLDER/*
 echo 'done.'
--- start.sh
+++ start.sh
@@ -4,12 +4,19 @@ cp /data/.ssh/id_rsa.pub /root/.ssh
 chmod 400 /root/.ssh/id_rsa /root/.ssh/id_rsa.pub
 
 echo -e "Host github.com\n\tStrictHostKeyChecking no\n" > ~/.ssh/config
-git clone --depth=1 $GITHUB ./github
+
 git config --global user.email $GIT_USER_EMAIL
 git config --global user.name $GIT_USER_NAME
+git config --global init.defaultBranch master
+
+mkdir ./github
+cd ./github
+git init
+git remote add origin $GITHUB
+git branch -M master
 
 echo "$CRON /data/cron.sh" >> /var/spool/cron/crontabs/root
 
-./cron.sh
+/data/cron.sh
 
 crond -f
