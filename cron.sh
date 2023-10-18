6d83cfee6242aa15ea83c17fcef4f280422ba78e
Mingfun Wong <mingfun.wong.chn@gmail.com>
2023-10-18 12:19:38 +0900

    fix upload header

--- cron.sh
+++ cron.sh
@@ -12,7 +12,7 @@ if [ ! -f $SNAPSHOT_FILE ];then
   exit 1
 fi
 
-curl -X PUT $UPLOAD_URL/$(date +%Y%m%d).bin -i -k --header "X-Custom-Auth-Key: $AUTH_KEY_SECRET" -F "data=@$SNAPSHOT_FILE"
-curl -X PUT $UPLOAD_URL/latest.bin -i -k --header "X-Custom-Auth-Key: $AUTH_KEY_SECRET" -F "data=@$SNAPSHOT_FILE"
+curl -X PUT $UPLOAD_URL/$(date +%Y%m%d).bin -i -k --header "X-Custom-Auth-Key: $AUTH_KEY_SECRET" --data-binary "@$SNAPSHOT_FILE"
+curl -X PUT $UPLOAD_URL/latest.bin -i -k --header "X-Custom-Auth-Key: $AUTH_KEY_SECRET" --data-binary "@$SNAPSHOT_FILE"
 rm -f $SNAPSHOT_FOLDER/*
 echo `date`"|end"
