5ad9c352788732949b9aa7d9cd1620e2928a9c4e
Mingfun Wong <mingfun.wong.chn@gmail.com>
2023-10-20 11:34:24 +0900

    add cron latest

--- cron.sh
+++ cron.sh
@@ -6,13 +6,19 @@ echo `date`"|start"
 wget $SNAPSHOT_URL -O /data/snapshot.log
 SNAPSHOT_FOLDER=/data/local/snapshots
 SNAPSHOT_FILE=$(ls $SNAPSHOT_FOLDER/*.bin -t | head -1)
+TARGET=$1
+UPLOAD_FILE_URL=$UPLOAD_URL/$(date +%Y%m%d).bin
 
 if [ ! -f $SNAPSHOT_FILE ];then
   echo "SNAPSHOT_FILE doesn't exist"
   exit 1
 fi
 
-curl -X PUT $UPLOAD_URL/$(date +%Y%m%d).bin -i -k --header "X-Custom-Auth-Key: $AUTH_KEY_SECRET" --data-binary "@$SNAPSHOT_FILE"
-curl -X PUT $UPLOAD_URL/latest.bin -i -k --header "X-Custom-Auth-Key: $AUTH_KEY_SECRET" --data-binary "@$SNAPSHOT_FILE"
+if [ $TARGET = "latest" ];then
+  UPLOAD_FILE_URL=$UPLOAD_URL/latest.bin
+fi
+
+curl -X PUT $UPLOAD_FILE_URL -i -k --header "X-Custom-Auth-Key: $AUTH_KEY_SECRET" --data-binary "@$SNAPSHOT_FILE"
+
 rm -f $SNAPSHOT_FOLDER/*
 echo `date`"|end"
--- start.sh
+++ start.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 set -e
 
-echo "$CRON sh /data/cron.sh" > /var/spool/cron/crontabs/root
+echo "" > /var/spool/cron/crontabs/root
+echo "$CRON_ARCHIVE sh /data/cron.sh archive" >> /var/spool/cron/crontabs/root
+echo "$CRON_LATEST sh /data/cron.sh latest" >> /var/spool/cron/crontabs/root
 
 sleep 10m
 
-sh /data/cron.sh
+sh /data/cron.sh latest
 
 crond -f
