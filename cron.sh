52de0aa7ace39467a3b095fb7bbaf06948b75865
Mingfun Wong <mingfun.wong.chn@gmail.com>
2024-02-23 18:49:58 +0900

    Update cron.sh

--- cron.sh
+++ cron.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 set -e
 
-echo `date`"|start"
+echo "$(date) Start"
+
+RETRY_COUNT=5
+RETRY_DELAY=5
 
 wget $SNAPSHOT_URL -O /data/snapshot.log
 SNAPSHOT_FOLDER=/data/local/snapshots
@@ -9,17 +12,28 @@ SNAPSHOT_FILE=$(ls $SNAPSHOT_FOLDER/*.bin -t | head -1)
 TARGET=$1
 UPLOAD_FILE_URL=$UPLOAD_URL/$(date +%Y%m%d).bin
 
-if [ ! -f $SNAPSHOT_FILE ];then
-  echo "SNAPSHOT_FILE doesn't exist"
+if [ ! -f $SNAPSHOT_FILE ]; then
+  echo "SNAPSHOT_FILE Doesn't exist"
   exit 1
 fi
 
-if [ $TARGET = "latest" ];then
+if [ $TARGET = "latest" ]; then
   UPLOAD_FILE_URL=$UPLOAD_URL/latest.bin
 fi
 
-echo `date`"|"$UPLOAD_FILE_URL
-curl -X PUT $UPLOAD_FILE_URL -i -k --header "X-Custom-Auth-Key: $AUTH_KEY_SECRET" --data-binary "@$SNAPSHOT_FILE"
+echo "$(date) | $UPLOAD_FILE_URL"
+
+# Retry loop
+for ((i=1; i<=$RETRY_COUNT; i++)); do
+  echo "$(date) Attempt $i"
+  if curl -X PUT $UPLOAD_FILE_URL -i -k --header "X-Custom-Auth-Key: $AUTH_KEY_SECRET" --data-binary "@$SNAPSHOT_FILE"; then
+    echo "$(date) Upload successful"
+    break
+  else
+    echo "$(date) Upload failed. Retrying in $RETRY_DELAY seconds..."
+    sleep $RETRY_DELAY
+  fi
+done
 
 rm -f $SNAPSHOT_FOLDER/*
-echo `date`"|end"
+echo "$(date) Done"
